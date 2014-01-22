a9598bbef594baee52d387fdef7d089ece0830dc
sagi2313 <sagialeko@gmail.com>
2014-01-22 10:41:41 -0800

    aaa

---
+++ GccApplication2/trunk/GccApplication2/GccApplication2.cpp
@@ -0,0 +1,17 @@
+/*
+ * GccApplication2.cpp
+ *
+ * Created: 1/22/2014 20:17:04
+ *  Author: Sagi
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
